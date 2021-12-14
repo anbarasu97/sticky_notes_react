cbcbb5cf4869471d71924d2b0142db5f9b48a93b
anbu <anbarasu2100@gmail.com>
2021-12-14 20:32:18 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = () => {
+    const newNote = {
+      id: String(Math.floor(Math.random() * 10000)),
+      content: "",
+    };
+    setNotes([...notes, newNote]);
+  };
+
+  const updateNotes = (idNote, contentNote) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id === idNote) {
+        note.content = contentNote;
+      }
+      return note;
+    });
+    setNotes([...updatedNotes]);
+    localStorage.setItem("sticky-notes", JSON.stringify(updatedNotes));
+  };
+
+  const deleteNotes = (idNote, contentNote) => {
+    const updatedNotes = notes.filter((note) => {
+      return note.id !== idNote;
+    });
+    setNotes([...updatedNotes]);
+    localStorage.setItem("sticky-notes", JSON.stringify(updatedNotes));
+  };
+
+  useEffect(() => {
+    setNotes(JSON.parse(localStorage.getItem("sticky-notes") || "[]"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {notes.map((note, index) => {
+        return (
+          <textarea
+            className="note"
+            onChange={(e) => updateNotes(e.target.id, e.target.value)}
+            onDoubleClick={(e) => deleteNotes(e.target.id, e.target.value)}
+            value={note.content}
+            key={index}
+            id={note.id}
+          />
+        );
+      })}
+
+      <button className="add-note" onClick={() => addNote()} type="button">
+        +
+      </button>
     </div>
   );
 }
